72256f935e2963e23b3678383f99b3aecbb16be8
Adrián Silva <adri4silva@gmail.com>
2016-12-12 09:11:14 +0100

    Segundo commit

--- Factorial.java
+++ Factorial.java
@@ -1,21 +1,36 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package factorial;
 
-/**
- *
- * @author Adri
- */
+
 public class Factorial {
 
-    /**
-     * @param args the command line arguments
-     */
+
     public static void main(String[] args) {
-        // TODO code application logic here
-    }
+
+
+    int j;
+    int f;
+
+  
+   j = 8;    
+   
+
     
+   int i;
+   if (j == 0)
+f = 1;
+   else
+   {
+f = 1;
+for (i = j; i >= 1; i--)
+{
+    f = f * i;
+}
+   }    
+  
+ 
+
+System.out.println(f); 
+
+
+    }
 }
