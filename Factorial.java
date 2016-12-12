908f29176eb12e80f572d579a41fc60f79aabe2c
Adrián Silva <adri4silva@gmail.com>
2016-12-12 09:19:46 +0100

    Sacado metodo calculaFactorial

--- Factorial.java
+++ Factorial.java
@@ -4,21 +4,22 @@ public class Factorial {
 
     public static void main(String[] args) {
 
-        int j, factorial;
+        int j = 8;
 
-        j = 8;
+        System.out.println(calculaFactorial(j));
 
-        int i;
+    }
+
+    public static int calculaFactorial(int j) {
+        int factorial, i;
         if (j == 0) {
             factorial = 1;
         } else {
             factorial = 1;
             for (i = j; i >= 1; i--) {
-                factorial = factorial * i;
+                factorial *= i;
             }
         }
-
-        System.out.println(factorial);
-
+        return factorial;
     }
 }
